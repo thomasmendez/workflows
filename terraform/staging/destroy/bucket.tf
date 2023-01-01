f28a9f27848313a6a0e5baf07fec8f7415db42ee
Thomas Mendez <thomasmendez01@gmail.com>
2023-01-01 10:59:43 -0600

    Added stg workflow

---
+++ terraform/staging/destroy/bucket.tf
@@ -0,0 +1,64 @@
+resource "aws_s3_bucket" "bucketstg" {
+  bucket        = var.aws_bucket_name
+  force_destroy = true
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucketstg" {
+  bucket = var.aws_bucket_name
+  acl    = "public-read"
+}
+
+# resource "aws_s3_bucket_lifecycle_configuration" "bucketstg" {
+#   bucket = aws_s3_bucket.bucketstg.id
+#   rule {
+#     id = "cleanup"
+#     filter {} # applies to all objects in bucket
+#     status = "Enabled"
+#     expiration {
+#       days = 1
+#     }
+#   }
+# }
+
+resource "aws_s3_bucket_policy" "bucketstg" {
+  bucket = var.aws_bucket_name
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.aws_bucket_name}/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket_website_configuration" "bucketstg" {
+  bucket = var.aws_bucket_name
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+data "aws_s3_bucket" "bucketstg" {
+  bucket = var.aws_bucket_name
+}
+
+output "bucketstg" {
+  value = data.aws_s3_bucket.bucketstg.bucket
+}
