7d535c43650cc45b16cc62d1a6dc0b494873879d
thomasmendez <thomasmendez01@gmail.com>
2023-02-24 23:37:21 -0600

    Added terraform lamda code infastructure for dev and destory

---
+++ terraform/development/lambda/create/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
+variable "aws_bucket_name" {
+  description = "S3 Bucket Name"
+  type        = string
+}
---
+++ terraform/development/lambda/destroy/bucket.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "bucketdev" {
+  bucket        = var.aws_bucket_name
+  force_destroy = true
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucketdev" {
+  bucket = var.aws_bucket_name
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucketdev" {
+  bucket = aws_s3_bucket.bucketdev.id
+  rule {
+    id = "cleanup"
+    filter {} # applies to all objects in bucket
+    status = "Enabled"
+    expiration {
+      days = 1
+    }
+  }
+}
+
+data "aws_s3_bucket" "bucketdev" {
+  bucket = var.aws_bucket_name
+}
+
+output "bucketdev" {
+  value = data.aws_s3_bucket.bucketdev.bucket
+}
