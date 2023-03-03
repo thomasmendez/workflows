0576f4731208a9f5749b3336ffc547098b32d0fd
thomasmendez <thomasmendez01@gmail.com>
2023-03-03 16:39:34 -0600

    Comment out bucket reference iam_role

--- terraform/development/lambda/create/lambda.tf
+++ terraform/development/lambda/create/lambda.tf
@@ -1,19 +1,24 @@
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name = "iam_for_lambda"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Sid    = ""
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#       }
+#     ]
+#   })
+# }
+
+data "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 }
+
 resource "aws_lambda_function" "lambda_dev" {
   s3_bucket     = var.aws_bucket_name
   s3_key        = var.lambda_function_s3_key
