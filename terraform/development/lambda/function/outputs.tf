f67320083bf80a2c5ee0185ad057e74dcfeef059
thomasmendez <thomasmendez01@gmail.com>
2023-03-17 02:25:33 -0500

    Added api gateway terraform file

--- terraform/development/lambda/function/outputs.tf
+++ terraform/development/lambda/function/outputs.tf
@@ -1,4 +1,5 @@
-output "lambda_dev" {
-  description = "Amazon Resource Name (ARN)"
-  value       = aws_lambda_function.lambda_dev.arn
+output "base_url" {
+  description = "Base URL for API Gateway stage"
+
+  value = aws_api_gateway_stage.apigateway_stage_dev.invoke_url
 }
