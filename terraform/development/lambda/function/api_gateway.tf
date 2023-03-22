b3bac38966906e16759460ed527a9232605492d2
thomasmendez <thomasmendez01@gmail.com>
2023-03-21 22:40:18 -0500

    Updated to not deploy api gateway due to terraform bug

--- terraform/development/lambda/function/api_gateway.tf
+++ terraform/development/lambda/function/api_gateway.tf
@@ -1,98 +1,101 @@
-resource "aws_api_gateway_rest_api" "api_gateway_dev" {
-  name = "${var.lambda_function_name}-api-gateway"
+# Everything works but duplicate api gateway is made
+# Issue: https://github.com/hashicorp/terraform-provider-aws/issues/14661
+# Also: https://github.com/hashicorp/terraform-provider-aws/issues/11344
+# resource "aws_api_gateway_rest_api" "api_gateway_dev" {
+#   name = "${var.lambda_function_name}-api-gateway"
 
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
+#   endpoint_configuration {
+#     types = ["REGIONAL"]
+#   }
+# }
 
-resource "aws_api_gateway_method" "root_method_dev" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway_dev.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "root_method_dev" {
+#   rest_api_id   = aws_api_gateway_rest_api.api_gateway_dev.id
+#   resource_id   = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_method_response" "root_method_response_dev" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
-  resource_id = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
-  http_method = aws_api_gateway_method.api_gateway_method_dev.http_method
-  status_code = "200"
+# resource "aws_api_gateway_method_response" "root_method_response_dev" {
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
+#   resource_id = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
+#   http_method = aws_api_gateway_method.api_gateway_method_dev.http_method
+#   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
+# }
 
-resource "aws_api_gateway_integration" "root_method_integration_dev" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway_dev.id
-  resource_id             = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
-  http_method             = aws_api_gateway_method.api_gateway_method_dev.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_dev.invoke_arn
-}
+# resource "aws_api_gateway_integration" "root_method_integration_dev" {
+#   rest_api_id             = aws_api_gateway_rest_api.api_gateway_dev.id
+#   resource_id             = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
+#   http_method             = aws_api_gateway_method.api_gateway_method_dev.http_method
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.lambda_dev.invoke_arn
+# }
 
-resource "aws_api_gateway_resource" "proxy_resource_dev" {
-  path_part   = "{proxy+}"
-  parent_id   = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
-}
+# resource "aws_api_gateway_resource" "proxy_resource_dev" {
+#   path_part   = "{proxy+}"
+#   parent_id   = aws_api_gateway_rest_api.api_gateway_dev.root_resource_id
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
+# }
 
-resource "aws_api_gateway_method" "api_gateway_method_dev" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway_dev.id
-  resource_id   = aws_api_gateway_resource.proxy_resource_dev.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "api_gateway_method_dev" {
+#   rest_api_id   = aws_api_gateway_rest_api.api_gateway_dev.id
+#   resource_id   = aws_api_gateway_resource.proxy_resource_dev.id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_method_response" "api_gateway_method_response_dev" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
-  resource_id = aws_api_gateway_resource.proxy_resource_dev.id
-  http_method = aws_api_gateway_method.api_gateway_method_dev.http_method
-  status_code = "200"
+# resource "aws_api_gateway_method_response" "api_gateway_method_response_dev" {
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
+#   resource_id = aws_api_gateway_resource.proxy_resource_dev.id
+#   http_method = aws_api_gateway_method.api_gateway_method_dev.http_method
+#   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
+# }
 
-resource "aws_api_gateway_integration" "integration_dev" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway_dev.id
-  resource_id             = aws_api_gateway_resource.proxy_resource_dev.id
-  http_method             = aws_api_gateway_method.api_gateway_method_dev.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_dev.invoke_arn
-}
+# resource "aws_api_gateway_integration" "integration_dev" {
+#   rest_api_id             = aws_api_gateway_rest_api.api_gateway_dev.id
+#   resource_id             = aws_api_gateway_resource.proxy_resource_dev.id
+#   http_method             = aws_api_gateway_method.api_gateway_method_dev.http_method
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.lambda_dev.invoke_arn
+# }
 
-resource "aws_lambda_permission" "api_gateway_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_dev.function_name
-  principal     = "apigateway.amazonaws.com"
+# resource "aws_lambda_permission" "api_gateway_lambda" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.lambda_dev.function_name
+#   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.api_gateway_dev.execution_arn}/*/*"
-}
+#   source_arn = "${aws_api_gateway_rest_api.api_gateway_dev.execution_arn}/*/*"
+# }
 
-resource "aws_api_gateway_deployment" "api_gateway_deployment_dev" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
+# resource "aws_api_gateway_deployment" "api_gateway_deployment_dev" {
+#   rest_api_id = aws_api_gateway_rest_api.api_gateway_dev.id
 
-  triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.proxy_resource_dev.id,
-      aws_api_gateway_method.root_method_dev.id,
-      aws_api_gateway_integration.integration_dev.id,
-    ]))
-  }
+#   triggers = {
+#     redeployment = sha1(jsonencode([
+#       aws_api_gateway_resource.proxy_resource_dev.id,
+#       aws_api_gateway_method.root_method_dev.id,
+#       aws_api_gateway_integration.integration_dev.id,
+#     ]))
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   # lifecycle {
+#   #   create_before_destroy = true
+#   # }
+# }
 
-resource "aws_api_gateway_stage" "apigateway_stage_dev" {
-  deployment_id = aws_api_gateway_deployment.api_gateway_deployment_dev.id
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway_dev.id
-  stage_name    = var.env
-}
+# resource "aws_api_gateway_stage" "apigateway_stage_dev" {
+#   deployment_id = aws_api_gateway_deployment.api_gateway_deployment_dev.id
+#   rest_api_id   = aws_api_gateway_rest_api.api_gateway_dev.id
+#   stage_name    = var.env
+# }
