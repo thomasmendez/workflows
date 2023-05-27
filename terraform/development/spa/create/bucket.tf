f5fbe459f326c0f2bd7e718125a2c2e25d5dadbe
thomasmendez <thomasmendez01@gmail.com>
2023-05-27 00:53:13 -0500

    Use the new public access block with ownership controls

--- terraform/development/spa/create/bucket.tf
+++ terraform/development/spa/create/bucket.tf
@@ -5,8 +5,29 @@ resource "aws_s3_bucket" "bucketdev" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucketdev" {
+  bucket = aws_s3_bucket.bucketdev.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucketdev" {
+  bucket = aws_s3_bucket.bucketdev.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "bucketdev" {
-  bucket = var.aws_bucket_name
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucketdev,
+    aws_s3_bucket_public_access_block.bucketdev,
+  ]
+
+  bucket = aws_s3_bucket.bucketdev.id
   acl    = "public-read"
 }
 
